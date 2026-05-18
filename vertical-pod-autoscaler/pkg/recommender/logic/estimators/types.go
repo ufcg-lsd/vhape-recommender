57cbfa25fb8023dd95a869a61ffc9104af66e340
Mapalmeira <matheus.palmeira.leite@gmail.com>
2026-05-18 18:04:33 -0300

    refactor: using another data type

--- vertical-pod-autoscaler/pkg/recommender/logic/estimators/types.go
+++ vertical-pod-autoscaler/pkg/recommender/logic/estimators/types.go
@@ -3,18 +3,18 @@ package estimators
 import "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/recommender/model"
 
 type ResourceRecommendation struct {
-	Target         model.Resource
-	LowerBound     model.Resource
-	UpperBound     model.Resource
-	UncappedTarget model.Resource
+	Target         model.ResourceAmount
+	LowerBound     model.ResourceAmount
+	UpperBound     model.ResourceAmount
+	UncappedTarget model.ResourceAmount
 }
 
 type ResourceConstraints struct {
-	Min model.Resource
-	Max model.Resource
+	Min model.ResourceAmount
+	Max model.ResourceAmount
 }
 
 type ResourceEstimator interface {
-	FeedSamples(containerName string, samples []model.Resource)
+	FeedSamples(containerName string, samples []model.ResourceAmount)
 	GetResourceRecommendation(containerName string, constraints ResourceConstraints) ResourceRecommendation
 }
