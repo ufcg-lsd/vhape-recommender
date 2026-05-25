306895d4161ec9af249700e7209283d4e0c306a0
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-05-25 15:06:50 -0300

    docs: docummenting recommendation types

--- vertical-pod-autoscaler/pkg/recommender/logic/recommendation/types.go
+++ vertical-pod-autoscaler/pkg/recommender/logic/recommendation/types.go
@@ -1,7 +1,20 @@
+/*
+Package recommendation defines recommendation value types shared by the
+recommender, estimators, and scaling rules.
+
+This package serves to avoid import cycles.
+*/
+
 package recommendation
 
 import "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/recommender/model"
 
+
+// SingleResourceRecommendation represents the recommendation calculated for a
+// single resource, such as CPU or memory.
+//
+// Estimators return this type because each estimator is responsible for one
+// resource at a time.
 type SingleResourceRecommendation struct {
 	Target         model.ResourceAmount
 	LowerBound     model.ResourceAmount
@@ -9,6 +22,9 @@ type SingleResourceRecommendation struct {
 	UncappedTarget model.ResourceAmount
 }
 
+
+// ResourceRecommendation represents the recommendation for all controlled
+// resources of a container.
 type ResourceRecommendation struct {
 	Target         model.Resources
 	LowerBound     model.Resources
