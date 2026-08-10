1c1bb380702bbd30f4f680b22d7d59081b1fa339
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-08-10 09:38:27 -0300

    feat: new CRDs for regex feature

--- vertical-pod-autoscaler/pkg/apis/autoscaling.vhape.io/v1alpha1/types.go
+++ vertical-pod-autoscaler/pkg/apis/autoscaling.vhape.io/v1alpha1/types.go
@@ -41,6 +41,54 @@ type VhapeWatchedNamespaceSpec struct {
 	VPAUpdateMode vpav1.UpdateMode `json:"vpaUpdateMode"`
 }
 
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VhapeWatchedNamespaceRegex marks namespaces matching Regex as eligible for VHAPE Watcher management.
+type VhapeWatchedNamespaceRegex struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec VhapeWatchedNamespaceRegexSpec `json:"spec,omitempty"`
+}
+
+// VhapeWatchedNamespaceRegexSpec describes a namespace regex and the VHAPE configuration applied to matching namespaces.
+type VhapeWatchedNamespaceRegexSpec struct {
+	VhapeWatchedNamespaceSpec `json:",inline"`
+	Regex                     string `json:"regex"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VhapeWatchedNamespaceRegexList is a list of VhapeWatchedNamespaceRegex objects.
+type VhapeWatchedNamespaceRegexList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []VhapeWatchedNamespaceRegex `json:"items"`
+}
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VhapeIgnoredNamespace marks a namespace that must be excluded from regex-based VHAPE Watcher management.
+type VhapeIgnoredNamespace struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VhapeIgnoredNamespaceList is a list of VhapeIgnoredNamespace objects.
+type VhapeIgnoredNamespaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []VhapeIgnoredNamespace `json:"items"`
+}
+
 type VhapePolicyRef struct {
 	Namespace string `json:"namespace"`
 	Name      string `json:"name"`
