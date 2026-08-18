caea44f6e571811905a54e9efdf600c407b6a977
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-08-18 10:06:23 -0300

    refactor: renaming heuristic

--- vertical-pod-autoscaler/pkg/vhapewatcher/testutil/constants.go
+++ vertical-pod-autoscaler/pkg/vhapewatcher/testutil/constants.go
@@ -8,7 +8,7 @@ const (
 	TestVPAName        = "vpa-api"
 
 	TestPolicyNamespace = "kube-system"
-	TestPolicyName      = "vhape-policy-p93-default"
+	TestPolicyName      = "p93-percentile-hysteresis"
 
 	TestVPAUpdateMode vpav1.UpdateMode = vpav1.UpdateModeInPlaceOrRecreate
 )
